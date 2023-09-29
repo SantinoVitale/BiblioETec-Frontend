f448299b9cf77d5657e52ba205412b41de7e3306
Ch1nolas <nmartinez252524342@gmail.com>
2023-09-29 11:33:25 -0300

    fix: deleteCard

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,7 +28,8 @@ function Home() {
     }).then( async (result) => {
       /* Read more about isConfirmed, isDenied below */
       if (result.isConfirmed) {
-        await axios.delete(`http://localhost:8080/api/booksManager/` + bid)
+        const res = await axios.delete(`http://localhost:8080/api/booksManager/` + bid)
+        console.log(res);
         const getData = await axios.get("http://localhost:8080/api/booksManager")
         const data = getData.data.payload.booksCard
         setList(data)
@@ -50,8 +51,8 @@ function Home() {
         <a href="#large-th"><i class="fa fa-th-large" aria-hidden="true">b</i></a>
       </div>
       </div>
-        {list.map((booksCard) => (
-        <div id="list-th">
+      <div className='flex flex-wrap' id="list-th">
+      {list.map((booksCard) => (
         <div className="book read">
           <div className="cover">
             <img src={booksCard.books.img} alt='imagen Libro'/>
@@ -62,13 +63,14 @@ function Home() {
             <p className="leading-relaxed mb-3 text-sm horario"> Fecha en la que se retiró: {format(new Date(booksCard.retiredDate), 'dd/MM/yyyy - HH:mm')} </p>
             <p className="leading-relaxed mb-3 text-sm horario"> Fecha de vencimiento: {format(new Date(booksCard.expireDate), 'dd/MM/yyyy - HH:mm')} </p>
             <div className="flex justify-center">
-              <Button className='mb-5' color='red' onClick={() => deleteBookCard(booksCard.books._id)}>Borrar</Button>
+              <Button className='mb-5' color='red' onClick={() => deleteBookCard(booksCard._id)}>Borrar</Button>
             </div>
             
           </div>
         </div>
-        </div>
         ))}
+      </div>
+        
         </div>
   );
 }
