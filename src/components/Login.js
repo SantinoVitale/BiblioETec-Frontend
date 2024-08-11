d2ef1c1c3563cfb9950752a57eec370e0c500c54
riveritoGithub <Ricardorivero.3257@gmail.com>
2024-08-11 01:01:03 -0300

    feat: email options in login/register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,28 @@
-import { Card, CardHeader, CardBody, CardFooter, Typography, Input, Button } from "@material-tailwind/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Button,
+  Select,
+  Option,
+} from "@material-tailwind/react";
 import axios from "axios";
 import { useContext, useState } from "react";
-import { toast } from 'react-hot-toast'
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/userContext";
 
 function Login() {
   const navigate = useNavigate();
-  const { setUser } = useContext(UserContext)
+  const { setUser } = useContext(UserContext);
+  const [value, setValue] = useState("@alumno.etec.um.edu.ar");
   const [data, setData] = useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: "",
+  });
   /*
   ! Ver si se implementa luego 
   const [showPassword, setShowPassword] = useState(false);
@@ -23,32 +34,30 @@ function Login() {
 
   const loginUser = async (e) => {
     e.preventDefault();
-    const { email , password } = data;
-    try
-    {
-      await axios.post("/api/users/login", {
-        email: email + "@alumno.etec.um.edu.ar",
-        password
-      })
-      .then((res) => 
-      {
-        toast.success("Logged successfully! Welcome to BiblioETec");
-        const newUser = {
-          ...res.data.payload,
-          id: res.data.payload._id
-        }
-        setUser(newUser)
-        setData({});
-        navigate("/");
-      })
-    } catch(error)
-    {
+    const { email, password } = data;
+    try {
+      await axios
+        .post("/api/users/login", {
+          email: email + value,
+          password,
+        })
+        .then((res) => {
+          toast.success("Logged successfully! Welcome to BiblioETec");
+          const newUser = {
+            ...res.data.payload,
+            id: res.data.payload._id,
+          };
+          setUser(newUser);
+          setData({});
+          navigate("/");
+        });
+    } catch (error) {
       toast.error(error.response.data.message);
     }
-  }
+  };
 
   return (
-    <div className='flex justify-center items-center m-[100px]'>
+    <div className="flex justify-center items-center m-[100px]">
       <Card className="w-96 shadow-2xl">
         <CardHeader
           variant="gradient"
@@ -61,12 +70,37 @@ function Login() {
         </CardHeader>
         <form onSubmit={loginUser}>
           <CardBody className="flex flex-col gap-4">
-              <div className="inline-flex items- w-full">
-                  <Input variant="standard" aria-autocomplete="email" formNoValidate className="w-full" label="Email" type="text" containerProps={{className: "min-w-0 icon-father"}} icon={<span className="w-full">@alumno.etec.um.edu.ar</span>} autoComplete="email" onChange={(e) => setData({...data, email: e.target.value})}/>
-              </div>
-              <div className="inline-flex items- w-full">
-                  <Input variant="standard" className="w-full" label="Contraseña" type="password" containerProps={{className: "min-w-0 icon-father"}} onChange={(e) => setData({...data, password: e.target.value})}/>
-              </div>
+            <div className="inline-flex items- w-full">
+              <Input
+                variant="standard"
+                aria-autocomplete="email"
+                formNoValidate
+                className="w-full"
+                label="Email"
+                type="text"
+                containerProps={{ className: "min-w-0 icon-father" }}
+                icon={
+                  <select className="border-white border-solid border focus:outline-gray-400" onChange={(val) => setValue(val.target.value)}>
+                    <option value="@alumno.etec.um.edu.ar">
+                      @alumno.etec.um.edu.ar
+                    </option>
+                    <option value="@etec.um.edu.ar">@etec.um.edu.ar</option>
+                  </select>
+                }
+                autoComplete="email"
+                onChange={(e) => setData({ ...data, email: e.target.value })}
+              />
+            </div>
+            <div className="inline-flex items- w-full">
+              <Input
+                variant="standard"
+                className="w-full"
+                label="Contraseña"
+                type="password"
+                containerProps={{ className: "min-w-0 icon-father" }}
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+              />
+            </div>
           </CardBody>
           <CardFooter className="pt-0">
             <Button variant="gradient" color="blue" fullWidth type="submit">
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,14 @@
-import { Card, CardHeader, CardBody, CardFooter, Typography, Input, Button, Option, Select } from "@material-tailwind/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Button,
+  Option,
+  Select,
+} from "@material-tailwind/react";
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
@@ -6,15 +16,16 @@ import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [data, setData] = useState({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    course: '',
-    email: '',
-    password: ''
-  })
+    firstName: "",
+    lastName: "",
+    phone: "",
+    course: "",
+    email: "",
+    password: "",
+  });
+  const [value, setValue] = useState("@alumno.etec.um.edu.ar");
   const navigate = useNavigate();
-  
+
   /*
   ! Ver si se implementa luego
 
@@ -27,36 +38,42 @@ function Register() {
   */
 
   const registerUser = async (e) => {
-    e.preventDefault()
-    const {firstName, lastName, course, phone, password, email} = data;
+    e.preventDefault();
+    const { firstName, lastName, course, phone, password } = data;
+    const email = data.email + value
+
     if (phone.length !== 10) {
-      toast.error('El número de teléfono debe tener 10 dígitos');
+      toast.error("El número de teléfono debe tener 10 dígitos");
       return;
     }
-      await axios.post('/api/users/register', {
-        firstName, lastName, course, phone, password, email
+    await axios
+      .post("/api/users/register", {
+        firstName,
+        lastName,
+        course,
+        phone,
+        password,
+        email,
       })
       .then((res) => {
-        if(!res.data.valid)
-          toast.error(res.data.message)
-        else
-        {
+        if (!res.data.valid) toast.error(res.data.message);
+        else {
           setData({
-            firstName: '',
-            lastName: '',
-            course: '',
-            phone: '',
-            password: '',
-            email: ''
+            firstName: "",
+            lastName: "",
+            course: "",
+            phone: "",
+            password: "",
+            email: "",
           });
-          toast.success('Register complete! Welcome to BiblioETec');
-          navigate("/login")
+          toast.success("Register complete! Welcome to BiblioETec");
+          navigate("/login");
         }
-      })
-  }
+      });
+  };
 
   return (
-    <div className='flex justify-center items-center m-[100px]'>
+    <div className="flex justify-center items-center m-[100px]">
       <Card className="w-96 shadow-2xl">
         <CardHeader
           variant="gradient"
@@ -69,10 +86,29 @@ function Register() {
         </CardHeader>
         <form onSubmit={registerUser}>
           <CardBody className="flex flex-col gap-4">
-            <Input variant="standard" label="Nombre" size="lg" onChange={(e) => setData({...data, firstName: e.target.value})}/>
-            <Input variant="standard" label="Apellido" size="lg" onChange={(e) => setData({...data, lastName: e.target.value})}/>
-            <Input variant="standard" label="Teléfono" size="lg" onChange={(e) => setData({...data, phone: e.target.value})}/>
-            <Select variant="standard" label="Curso" onChange={(e) =>setData({...data, course: e})}>
+            <Input
+              variant="standard"
+              label="Nombre"
+              size="lg"
+              onChange={(e) => setData({ ...data, firstName: e.target.value })}
+            />
+            <Input
+              variant="standard"
+              label="Apellido"
+              size="lg"
+              onChange={(e) => setData({ ...data, lastName: e.target.value })}
+            />
+            <Input
+              variant="standard"
+              label="Teléfono"
+              size="lg"
+              onChange={(e) => setData({ ...data, phone: e.target.value })}
+            />
+            <Select
+              variant="standard"
+              label="Curso"
+              onChange={(e) => setData({ ...data, course: e })}
+            >
               <Option value="1°A">1°A</Option>
               <Option value="1°B">1°B</Option>
               <Option value="2°A">2°A</Option>
@@ -87,29 +123,49 @@ function Register() {
               <Option value="6°E">6°E</Option>
             </Select>
             <div className="inline-flex items- w-full">
-                  <Input variant="standard" className="w-full" label="Email" type="email" containerProps={{className: "min-w-0 icon-father"}} icon={<span className="w-full">@alumno.etec.um.edu.ar</span>} autoComplete="email" onChange={(e) => setData({...data, email: e.target.value})}/>
+              <Input
+                variant="standard"
+                className="w-full"
+                label="Email"
+                type="text"
+                containerProps={{ className: "min-w-0 icon-father" }}
+                icon={
+                  <select className="border-white border-solid border focus:outline-gray-400" onChange={(val) => setValue(val.target.value)}>
+                    <option value="@alumno.etec.um.edu.ar">@alumno.etec.um.edu.ar</option>
+                    <option value="@etec.um.edu.ar">@etec.um.edu.ar</option>
+                  </select>
+                }
+                autoComplete="email"
+                onChange={(e) => setData({ ...data, email: e.target.value })}
+              />
             </div>
             <div className="inline-flex items- w-full">
-                  <Input variant="standard" className="w-full" label="Contraseña" type="password" containerProps={{className: "min-w-0 icon-father"}} onChange={(e) => setData({...data, password: e.target.value})}/>
+              <Input
+                variant="standard"
+                className="w-full"
+                label="Contraseña"
+                type="password"
+                containerProps={{ className: "min-w-0 icon-father" }}
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+              />
             </div>
-
           </CardBody>
           <CardFooter className="pt-0">
             <Button variant="gradient" color="blue" fullWidth type="submit">
               Registrarse
             </Button>
             <Typography variant="small" className="mt-6 flex justify-center">
-            ¿Ya tienes cuenta?
-            <Typography
-              as="a"
-              href="/login"
-              variant="small"
-              color="blue-gray"
-              className="ml-1 font-bold"
-            >
-              Ingresar
+              ¿Ya tienes cuenta?
+              <Typography
+                as="a"
+                href="/login"
+                variant="small"
+                color="blue-gray"
+                className="ml-1 font-bold"
+              >
+                Ingresar
+              </Typography>
             </Typography>
-          </Typography>
           </CardFooter>
         </form>
       </Card>
